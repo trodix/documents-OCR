271579e32cd25f21fd2822dac1e54065b1b84d3a
Sebastien Vallet <sebastien.vallet89@gmail.com>
2025-09-13 14:09:55 +0200

    initial commit

---
+++ src/main/java/fr/cpage/ged/ia/iaocr/IaOcrApplication.java
@@ -0,0 +1,13 @@
+package fr.cpage.ged.ia.iaocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaOcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/cpage/ged/ia/iaocr/IaOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cpage.ged.ia.iaocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
